7362bc9832ad8cf9c2447ac815aa5a7bbe14afec
Mandana <mandanakhademi88@gmail.com>
2019-05-13 17:01:12 +0100

    convert like component to stateless functional component

--- src/components/common/like.jsx
+++ src/components/common/like.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 
-class Like extends Component {
-    render() {
-        let classes = "fa fa-heart";
-        if (!this.props.liked) classes += "-o";
-        return <i onClick={this.props.onClick} style={{ cursor: "pointer" }} className={classes} ></i>;
-    }
+const Like = props => {
+    let classes = "fa fa-heart";
+    if (!props.liked) classes += "-o";
+    return (
+        <i
+            onClick={props.onClick}
+            style={{ cursor: "pointer" }}
+            className={classes}
+        />
+
+    );
 }
+
 
 export default Like;
