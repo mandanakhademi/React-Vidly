083700d7babb2bbe75fa3d6f8645a3bc07682b09
Mandana <mandanakhademi88@gmail.com>
2019-05-20 11:52:52 +0100

    extract MoviesTable

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
+import MoviesTable from './moviesTable';
 import { getMovies } from '../services/fakeMovieService';
 import { getGenres } from '../services/fakeGenreService';
-import Like from "./common/like";
 import ListGroup from "./common/listGroup";
 import Pagination from "./common/pagination";
 import { paginate } from "../utils/paginate";
@@ -68,33 +68,7 @@ class Movies extends Component {
         </div>
         <div className="col">
           <p>Showing {count} movies in the database.</p>
-          <table className="table">
-            <thead>
-              <tr>
-                <th>Title</th>
-                <th>Genre</th>
-                <th>Stock</th>
-                <th>Rate</th>
-                <th></th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              {movies.map(movie => (
-                <tr key={movie._id}>
-                  <td>{movie.title}</td>
-                  <td>{movie.genre.name}</td>
-                  <td>{movie.numberInStock}</td>
-                  <td>{movie.dailyRentalRate}</td>
-                  <td>
-                    <Like liked={movie.liked} onClick={() => this.handleLike(movie)} />
-                  </td>
-                  <th><button onClick={() => this.handleDelete(movie)} className="btn btn-danger sm">Delete</button></th>
-                </tr>
-              ))}
-
-            </tbody>
-          </table>
+          <MoviesTable movies={movies} onLike={this.handleLike} onDelete={this.handleDelete} />
           <Pagination
             itemsCount={count}
             pageSize={pageSize}
@@ -105,9 +79,6 @@ class Movies extends Component {
 
       </div>
     );
-
-
-
 
 
   }
