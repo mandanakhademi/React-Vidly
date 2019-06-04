6c85a63e79f0b38f7179ff072ae9339eacd1159d
Mandana <mandanakhademi88@gmail.com>
2019-06-04 17:35:18 +0100

    add a basic validation function and display validation errors

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
 import Input from './common/input';
+import { objectTypeSpreadProperty } from '@babel/types';
 
 class LoginForm extends Component {
     state = {
-        account: { username: '', password: '' }
+        account: { username: '', password: '' },
+        errors: {}
     }
+
+    validate = () => {
+        const errors = {};
+
+        const { account } = this.state;
+        if (account.username.trim() === '')
+            errors.username = 'Username is required.';
+        if (account.password.trim() === '')
+            errors.password = 'Password is required.';
+
+        return Object.keys(errors).length === 0 ? null : errors;
+
+    };
 
     handleSubmit = e => {
         e.preventDefault();
+
+        const errors = this.validate();
+        this.setState({ errors: errors || {} });
+        if (errors) return;
 
         //cal the server
         console.log('submitted');
@@ -20,7 +39,7 @@ class LoginForm extends Component {
     }
 
     render() {
-        const { account } = this.state;
+        const { account, errors } = this.state;
         return (
             <div>
                 <h1>Login</h1>
@@ -30,12 +49,14 @@ class LoginForm extends Component {
                         value={account.username}
                         label="Username"
                         onChange={this.handleChange}
+                        error={errors.username}
                     />
                     <Input
                         name="password"
                         value={account.password}
                         label="Password"
                         onChange={this.handleChange}
+                        error={errors.password}
                     />
                     <button className="btn btn-primary">Login</button>
                 </form>
