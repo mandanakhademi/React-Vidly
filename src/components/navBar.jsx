75b3d39bf687d685aeeab5e20d6ae73181430b0b
Mandana <mandanakhademi88@gmail.com>
2019-06-08 16:29:20 +0100

    build register form

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -40,6 +40,11 @@ const NavBar = () => {
                             Login
                         </NavLink>
                     </li>
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="/Register">
+                            Register
+                        </NavLink>
+                    </li>
 
                 </ul>
             </div>
